4da5f84a76bc504818d2b554d616ed4a04ce0157
alex-jesus <minoalex.tigro20@gmail.com>
2026-05-26 18:55:17 -0300

    Update and rename seleccion.txt to seleccion_sizeof .c

--- trabajos del aula virtual/seleccion_sizeof .c
+++ trabajos del aula virtual/seleccion_sizeof .c
@@ -2,7 +2,11 @@
 
 int main() {
     int arr[] = {7, 4, 2, 9, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);//calcula cuantos elementos tiene el array 
+    //sizeof(arr): Mide el tamaño de todo el array. 
+    //Como tenés 5 números enteros y cada uno ocupa 4 bytes, esto da un total de 20 bytes 
+    //sizeof(arr[0]): Mide el tamaño de un solo elemento del array (el primero, que es un int). Eso da 4 bytes.
+    //La división (20 / 4): Al dividir el total de bytes por lo que ocupa un solo número, el resultado de n es 5.
     int i, j, min, temp;
 
     for (i = 0; i < n - 1; i++) {
